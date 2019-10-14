a1417cd6f41036345e0c87aba8f1e5d9dc1e977f
Shergot <martieh@stud.ntnu.no>
2019-10-14 16:34:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import charlieplexer as led
 import keypad
 
 def run():
-    password_path = ""
-    keypad = keypad.Keypad()
+    password_path = "./pass.txt"
+    pad = keypad.Keypad()
     ledboard = led.LEDBoard()
     ledboard.setup()
-    kpc = KPCAgent.KPCAgent(keypad, ledboard, password_path)
+    kpc = KPCAgent.KPCAgent(pad, ledboard, password_path)
     thefsm = fsm.Fsm(kpc)
     #Add fsm rules
     thefsm.addRule("S-init", "S-read", fsm.signal_is_symbol, kpc.reset_password_accumulator)
@@ -17,13 +17,13 @@ def run():
     thefsm.addRule("S-read", "S-init", fsm.signal_is_symbol, kpc.reset_agent)
     thefsm.addRule("S-verify", "S-active", "Y", kpc.fully_activate_agent)
     thefsm.addRule("S-verify", "S-init", fsm.signal_is_symbol, kpc.reset_agent)
-    thefsm.addRule("S-active", "S-led", fsm.signal_less_than_six(), ???) # sett led
-    thefsm.addRule("S-active", "S-password", "*", ???) # Begynn passordendring
+    thefsm.addRule("S-active", "S-led", fsm.signal_less_than_six(), kpc.set_Lid) # sett led
+    thefsm.addRule("S-active", "S-password", "*", None) # Begynn passordendring
     thefsm.addRule("S-active", "S-logout", "#", None)
-    thefsm.addRule("S-led", "S-time", "*", ???) #Begynn Sett tid, kanskje None
-    thefsm.addRule("S-time", "S-time", fsm.signal_is_digit, ???) #Legg til på tid
+    thefsm.addRule("S-led", "S-time", "*", None) #Begynn Sett tid, kanskje None
+    thefsm.addRule("S-time", "S-time", fsm.signal_is_digit, kpc.append_next_time_digit) #Legg til på tid
     thefsm.addRule("S-time", "S-active", "*", kpc.light_one_led)
-    thefsm.addRule("S-password", "S-password", fsm.signal_is_digit, ???) #Legg til på endret passord
+    thefsm.addRule("S-password", "S-password", fsm.signal_is_digit, kpc.append_next_password_digit) #Legg til på endret passord
     thefsm.addRule("S-password", "S-active", "*", kpc.validate_password)
     thefsm.addRule("S-logout", "S-done", "#", kpc.exit_action)
     thefsm.addRule("S-logout", "S-active", fsm.signal_is_symbol, None)
